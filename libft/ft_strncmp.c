71e3e2f3768aaf4165681853102625f80ed563b1
Fashin <cyprian.beauvois@gmail.com>
2016-11-06 14:44:35 +0100

    Add files via upload
    Just create ft_strncmp

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: cbeauvoi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/06 13:55:46 by cbeauvoi          #+#    #+#             */
-/*   Updated: 2016/11/06 14:24:24 by cbeauvoi         ###   ########.fr       */
+/*   Updated: 2016/11/06 14:41:18 by cbeauvoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,19 @@
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	return (s1[n - 1] - s2[n - 1]);
+	size_t	i;
+
+	i = 0;
+	if (s1 || s2 || n)
+	{
+		while (i < n)
+		{
+			if (s1[i] != s2[i])
+				return (s1[i] - s2[i]);
+			else if (!s2[i] || !s2[i])
+				return (0);
+			i++;
+		}
+	}
+	return (0);
 }
