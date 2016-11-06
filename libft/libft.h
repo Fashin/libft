57d7301e2aa592ec59d6f1c170aeb70fb2dadeae
Fashin <cyprian.beauvois@gmail.com>
2016-11-06 14:33:06 +0100

    Add files via upload
    First upload from 42 of my libft, i stop at strncmp

---
+++ libft/libft.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbeauvoi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/06 12:41:11 by cbeauvoi          #+#    #+#             */
+/*   Updated: 2016/11/06 14:00:52 by cbeauvoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <stdlib.h>
+#include <string.h>
+
+/*includes pour test les fonctions*/
+#include <ctype.h>
+#include <stdio.h>
+/*********************************/
+
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_atoi(const char *str);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+int		ft_isprint(int c);
+int		ft_isascii(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+
+#endif
